d16335db8425bc99fe339a80c5f77a46e1fde763
maximka777 <maksim.borovskij@yandex.ru>
2016-11-27 13:31:35 +0300

    NUnit expreiments

---
+++ DTOMapper/DTOMapper.UnitTests/MapperTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOMapper.UnitTests
+{
+    [TestFixture]
+    class MapperTests
+    {
+        [Test]
+        public void Map_CompatibleTypes_ReturnsMappedObject()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Map_CompatibleTypes_ReturnsMappedObject2()
+        {
+            Assert.True(true);
+        }
+    }
+}
